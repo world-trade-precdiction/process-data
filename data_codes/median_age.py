3be304a22df0498885651295e579e7c5b53e1192
applettuce <130147092+applettuce@users.noreply.github.com>
2025-07-27 16:39:32 +0900

    Update median_age.py
    median age description

--- data_codes/median_age.py
+++ data_codes/median_age.py
@@ -6,10 +6,10 @@ url = "https://ourworldindata.org/grapher/median-age.csv"
 resp = requests.get(url)
 resp.raise_for_status()
 
-df = pd.read_csv(StringIO(resp.text))
+df = pd.read_csv(StringIO(resp.text)) #csv 파일 읽기
 
 filtered_df = df[(df["Year"] >= 2000) & (df["Year"] <= 2021)]
 filtered_df_pivot = filtered_df.pivot(index = 'Entity', columns = 'Year', values = 'Median age - Sex: all - Age: all - Variant: estimates')
 filtered_df_pivot.index.name = 'Country'
 filtered_df_pivot.columns.name = 'Year'
-filtered_df_pivot.to_csv("median_age_2000_2021.csv", index=True, index_label="Country", encoding="utf-8-sig")
+filtered_df_pivot.to_csv("median_age_2000_2021.csv", index=True, index_label="Country", encoding="utf-8-sig") #csv 파일로 정리해서 만들기
